6fad617bdaaa4fb9ce8845548e2030677220b64d
Armando <armandoramossanchez@gmail.com>
2021-11-02 09:51:13 +0100

    añado seeders correctos de pedidos

--- seeders/03-demo-pedido.js
+++ seeders/03-demo-pedido.js
@@ -4,9 +4,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('pedidos', 
     [
-      {"peliculaId":15,"usuarioId":12,"fecha_alquiler":"2021-10-14","fecha_devolucion":"2021-10-27","createdAt":"2021-10-28","updatedAt":"2021-10-28"},
-      {"peliculaId":1,"usuarioId":3,"fecha_alquiler":"2021-10-09","fecha_devolucion":"2021-10-20","createdAt":"2021-10-28","updatedAt":"2021-10-28"},
-      {"peliculaId":3,"usuarioId":2,"fecha_alquiler":"2021-10-15","fecha_devolucion":"2021-10-26","createdAt":"2021-10-28","updatedAt":"2021-10-28"}
+      {"peliculaId":5,"usuarioId":15,"fecha_alquiler":"2021-10-09","fecha_devolucion":"2021-10-20","createdAt":"2021-10-28","updatedAt":"2021-10-28"},
+      {"peliculaId":15,"usuarioId":10,"fecha_alquiler":"2021-10-15","fecha_devolucion":"2021-10-26","createdAt":"2021-10-28","updatedAt":"2021-10-28"}
     ], {});
   },
 
