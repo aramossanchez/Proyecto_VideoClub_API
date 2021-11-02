4d39e90333fd663750326e4c879cdfcf7af9b27b
Armando <armandoramossanchez@gmail.com>
2021-11-02 19:57:19 +0100

    inicio de implementacion de swagger

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const logger = require('./config/winston');
 const db = require('./db.js');
 const router = require('./router.js');
 const cors = require("cors");
+//IMPORTAMOS SWAGGER, PARA DOCUMENTAR API EN LA PLATAFORMA DE DESPLIEGUE
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 const app = express();
 const PORT = process.env.PORT || 3000; //CONFIGURO PUERTO HEROKU
@@ -16,10 +19,24 @@ var corsOptions = {//CONFIGURO OPCIONES DE CORS
     optionsSuccessStatus: 204
 };
 
+//CONFIGURO SWAGGER DOC
+const swaggerDoc = swaggerJSDoc({
+    swaggerDefinition:{
+        info:{
+            title: "Proyecto VideoClub API",
+            version: "1.0.0",
+            description: "Documentación de la API"
+        }
+    },
+    apis: ["index.js"]
+})
+
+
 //Middleware --> FUNCIONES QUE SE EJECUTARÁN SIEMPRE ANTES DE CUALQUIER ACCIÓN QUE HAGAMOS DE APP
 app.use(morgan('combined', { stream: logger.stream }));//GENERO LOGS
 app.use(express.json()); //PUEDO OBTENER JSON DEL BODY
 app.use(cors(corsOptions));  //USO CORS
+app.use('/documentacion', swaggerUI.serve,swaggerUI.setup(swaggerDoc))//EN LA URL /documentacion PODEMOS VER LA DOCUMENTACION CREADA CON SWAGGER
 
 //RUTAS
 app.get('/', (req, res) => {res.send('Bienvenidos a nuestro VideoClub');}); //PANTALLA DE BIENVENIDA EN PANTALLA DE INICIO
